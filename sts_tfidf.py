50c09de41bed7247b44c81c510577a291a7f9c4a
emmerkhofer <emm228@georgetown.edu>
2021-02-23 20:00:55 -0500

    other lab solution

--- sts_tfidf.py
+++ sts_tfidf.py
@@ -15,7 +15,13 @@ def preprocess_text(text):
     """Preprocess one sentence: tokenizes, lowercases, applies the Porter stemmer,
      removes punctuation tokens and stopwords.
      Returns a string of tokens joined by whitespace."""
-    return " ".join(text)
+    stemmer = PorterStemmer()
+    stops = set(stopwords.words('english'))
+    toks = word_tokenize(text)
+    toks_stemmed = [stemmer.stem(tok.lower()) for tok in toks]
+    toks_nopunc = [tok for tok in toks_stemmed if tok not in string.punctuation]
+    toks_nostop = [tok for tok in toks_nopunc if tok not in stops]
+    return " ".join(toks_nostop)
 
 
 def main(sts_data):
@@ -23,13 +29,39 @@ def main(sts_data):
     texts, labels = parse_sts(sts_data)
 
     # TODO 1: get a single list of texts to determine vocabulary and document frequency
+
+    # get a single list of texts to determine vocabulary and document frequency
+    all_t1, all_t2 = zip(*texts)
+    print(len(all_t1))
+    all_texts = all_t1 + all_t2
+
     # create a TfidfVectorizer
     # fit to the training data
+    vectorizer = TfidfVectorizer("content", lowercase=True, analyzer="word", use_idf=True, min_df=10)
+    vectorizer.fit(all_texts)
 
+    print("Checking the vocabulary")
+    term_vocab = vectorizer.get_feature_names()
+    print(term_vocab[200:230])
+
+    print("Exploring sentence representations created by the vectorizer")
+    pair_reprs = vectorizer.transform(texts[0])
+    # a sparse datatype - saves only which positions are nonzero (where words are observed)
+    print(type(pair_reprs))
+    print(pair_reprs)
+    # compare the two representations
+    pair_similarity = cosine_similarity(pair_reprs[0], pair_reprs[1])
+    # similarity is returned in a matrix - have to get the right index to get a scalar
+    print(pair_similarity.shape)
+    print(pair_similarity[0, 0])
 
     # TODO 2: Can normalization like removing stopwords remove differences that aren't meaningful?
     # fill in preprocess_text above
-    preproc_train_texts = [preprocess_text(text) for text in texts]
+    preproc_train_texts = [preprocess_text(text) for text in all_texts]
+
+    preproc_vectorizer = TfidfVectorizer("content", lowercase=True, analyzer="word",
+                                         token_pattern="\S+", use_idf=True, min_df=10)
+    preproc_vectorizer.fit(preproc_train_texts)
 
     # TODO 3: Learn another TfidfVectorizer for preprocessed data
     # Use token_pattern "\S+" in the TfidfVectorizer to split on spaces
@@ -40,12 +72,22 @@ def main(sts_data):
 
     for pair in texts:
         t1,t2 = pair
+        pair_reprs = vectorizer.transform([t1,t2])
+        pair_similarity = cosine_similarity(pair_reprs[0], pair_reprs[1])
+        cos_sims.append(pair_similarity[0,0])
+
+        t1_preproc = preprocess_text(t1)
+        t2_preproc = preprocess_text(t2)
+        pair_reprs = preproc_vectorizer.transform([t1_preproc, t2_preproc])
+        pair_similarity = cosine_similarity(pair_reprs[0], pair_reprs[1])
+        cos_sims_preproc.append(pair_similarity[0,0])
 
     # TODO 5: measure the correlations
-    pearson = 0.0
-    preproc_pearson = 0.0
-    print(f"default settings: r={pearson:.03}")
-    print(f"preprocessed text: r={preproc_pearson:.03}")
+    pearson = pearsonr(cos_sims, labels)
+    preproc_pearson = pearsonr(cos_sims_preproc, labels)
+    print(f"default settings: r={pearson[0]:.03}")
+    print(f"preprocessed text: r={preproc_pearson[0]:.03}")
+
 
 
 
